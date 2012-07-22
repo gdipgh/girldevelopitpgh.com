6f8ad97d97fb413671ca1032c28153f0198827f6
Julie Pagano <julie.pagano@gmail.com>
2012-07-21 23:05:56 -0400

    Extra js to improve comic navigation and responsiveness

--- javascripts/comic.js
+++ javascripts/comic.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+  var max_width = Math.min(703, $(window).width() - 120);
+
+  if(Modernizr.mq('only screen and (-webkit-min-device-pixel-ratio: 1.5)') ||
+     Modernizr.mq('only screen and (min-device-pixel-ratio: 1.5)')) {
+    var retina_src = $("#comic").attr("src").replace(".png", "@2x.png");
+    $("#comic").attr("src", retina_src);
+  }
+
+
   $('img#comic').imgplayer({
     maxHeight: 471,
-    maxWidth: 703,
-    scalable: false,
+    maxWidth: max_width,
+    scalableUp: false,
+    scalableDown: true,
     frames: [
       [5, 5, 212, 240, {}],
       [5, 219, 241, 240, {}],
@@ -14,5 +24,13 @@ $(document).ready(function() {
       [244, 458, 241, 205, {}],
       [0, 0, 703, 471, {}]
     ]
+  });
+
+  $("#comic-left").on("click", function() {
+    $.imgplayer.gotoPrev();
+  });
+
+  $("#comic-right").on("click", function() {
+    $.imgplayer.gotoNext();
   });
 });
